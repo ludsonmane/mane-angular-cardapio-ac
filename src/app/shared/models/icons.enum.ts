d056f4315c897e89aba187ebed65f67826be09fa
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-11 23:02:06 -0300

    feat: cria enum para os icones

---
+++ src/app/shared/models/icons.enum.ts
@@ -0,0 +1,11 @@
+export enum IconsEnum {
+    Arrow = 'arrow',
+    Comma = 'comma',
+    Favorite = 'favorite',
+    Marker = 'marker',
+    NoLactose = 'no-lactose',
+    Options = 'options',
+    Restaurant = 'restaurant',
+    Search = 'search',
+    Vegetarian  = 'vegetarian'
+}
