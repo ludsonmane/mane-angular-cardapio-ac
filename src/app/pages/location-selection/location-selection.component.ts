96f7afcc04d1a463787dc2aab318da27a74f4bc8
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-14 01:03:48 -0300

    feat: adiciona lista de unidades

--- src/app/pages/location-selection/location-selection.component.ts
+++ src/app/pages/location-selection/location-selection.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocationsModel } from '../../shared/models/locations.model';
+import { LocationDataService } from './services/location-data/location-data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-location-selection',
@@ -6,6 +9,23 @@ import { Component } from '@angular/core';
     templateUrl: './location-selection.component.html',
     styleUrl: './location-selection.component.css'
 })
-export class LocationSelectionComponent {
+export class LocationSelectionComponent implements OnInit {
 
+    locations: LocationsModel[] = []
+
+    constructor(
+        private locationDataService: LocationDataService,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+        this.locationDataService.getLocations()
+            .subscribe((response: any) => { this.locations = response })
+    }
+
+    selectLocation(location: string): void {
+        this.router.navigate(['/home'], {
+            queryParams: { unitLocaton: location }
+        })
+    }
 }
