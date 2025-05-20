1011b914dfe9f5ad1c2a5549cf6fddbe75222a99
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-19 22:19:49 -0300

    feat: adiciona função ao clicar no item do menu

--- src/app/shared/components/menu-item-card/menu-item-card.component.ts
+++ src/app/shared/components/menu-item-card/menu-item-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-menu-item-card',
@@ -7,10 +8,18 @@ import { Component, Input } from '@angular/core';
     styleUrl: './menu-item-card.component.css'
 })
 export class MenuItemCardComponent {
+    @Input() id!: number
     @Input() imageUrl!: string
     @Input() title!: string
     @Input() description!: string
     @Input() currentPrice!: number
     @Input() originalPrice?: number
     @Input() isFavorite: boolean = false
+
+    constructor(private router: Router) {}
+
+    onNavigate(): void {
+        // Passar a rota da página menu-item-detail, quando for criada
+        this.router.navigate(['', this.id])
+    }
 }
