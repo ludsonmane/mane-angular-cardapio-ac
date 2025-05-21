cd11963cb76e1be4214e07b767fab9b35c172ddd
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-21 08:08:17 -0300

    feat: pega os dados do produto por id no componetn product.component

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,7 @@
-import { Component, HostListener } from '@angular/core';
+import { ProductService } from './../../shared/services/product/product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { ProductModel } from '../../shared/models/product.model';
 
 @Component({
     selector: 'app-product',
@@ -6,12 +9,30 @@ import { Component, HostListener } from '@angular/core';
     templateUrl: './product.component.html',
     styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
+
+    product!: ProductModel
+    productId!: string
 
     scrollY: number = 0
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private productService: ProductService
+    ) {}
+
+    ngOnInit(): void {
+        this.productId = this.activatedRoute.snapshot.paramMap.get('id') || ''
+        this.getDetailsProduct(this.productId)
+    }
 
     @HostListener('window:scroll', [])
     onScroll(): void {
         this.scrollY = window.scrollY
+    }
+
+    getDetailsProduct(id: string): void {
+        this.productService.getProductById(id)
+            .subscribe((response) => { if (response) this.product = response })
     }
 }
