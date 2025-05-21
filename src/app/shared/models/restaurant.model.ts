321c39c2b59fdc78210ebd56201fca4eec0802c2
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-21 13:01:07 -0300

    feat: cria modelo para os restaurantes

---
+++ src/app/shared/models/restaurant.model.ts
@@ -0,0 +1,11 @@
+export interface RestaurantModel {
+    id: string
+    locationId: string
+    name: string
+    description: string
+    imageLogoUrl: string
+    imageAboutUrl: string
+    about: string
+    color: string
+    chefTips: string[]
+}
