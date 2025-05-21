1bfe1945ef5927aec9f403c72d8a194f85057c2c
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-20 22:53:10 -0300

    feat: adiciona titulo ao componente header-actions

--- src/app/shared/components/header-actions/header-actions.component.ts
+++ src/app/shared/components/header-actions/header-actions.component.ts
@@ -10,7 +10,9 @@ export class HeaderActionsComponent {
     @Input() showFavorite: boolean = false
     @Input() showShare: boolean = false
     @Input() showInfo: boolean = false
+    @Input() showTitle: boolean = false
     @Input() isFavorite?: boolean = false
+    @Input() title?: string
 
     @Output() back = new EventEmitter<void>()
     @Output() favorite = new EventEmitter<void>()
