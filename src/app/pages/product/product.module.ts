e6c00537299d9e48cb065295e578d12f641f1f07
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-20 22:19:07 -0300

    feat: importa módulo Shared ao módulo Product

--- src/app/pages/product/product.module.ts
+++ src/app/pages/product/product.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
 import { ProductMaterialModule } from '../../shared/materials/product-material.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { ProductMaterialModule } from '../../shared/materials/product-material.m
         CommonModule,
         ProductRoutingModule,
         ProductMaterialModule,
+        SharedModule
     ]
 })
 export class ProductModule { }
