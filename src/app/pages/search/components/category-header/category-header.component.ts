2f0c7196541e75e51713b756856b32c948a0dfa2
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-05-24 18:55:04 -0300

    feat: cria componente para o header da página Produtos pro categoria

---
+++ src/app/pages/search/components/category-header/category-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-category-header',
+    standalone: false,
+    templateUrl: './category-header.component.html',
+    styleUrl: './category-header.component.css'
+})
+export class CategoryHeaderComponent {
+    @Input() title!: string
+    @Input() imageUrl!: string
+    @Input() scrolled: boolean = false
+
+    goBack(): void {}
+}
