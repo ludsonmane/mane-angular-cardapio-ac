75cdba962489b652a024f3f7c28a6d20a13dee83
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-06-20 14:33:10 -0300

    fix: somente algumas páginas mostram o menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IconService } from './shared/services/icon/icon.service';
 import { LanguageService } from './shared/services/language/language.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -10,10 +12,29 @@ import { LanguageService } from './shared/services/language/language.service';
 })
 export class AppComponent implements OnInit {
 
+    showMenu: boolean = false
+
     constructor(
         private iconService: IconService,
-        private languageService: LanguageService
-    ) {}
+        private router: Router
+    ) {
+        this.router.events
+            .pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe((event) => {
+                const url =  (event as NavigationEnd).urlAfterRedirects
+
+                // Rotas que devem mostrar o menu
+                const routesWithMenu = [
+                    '/home',
+                    '/restaurantes',
+                    '/buscar',
+                    '/favoritos',
+                    '/opcoes'
+                ]
+
+                this.showMenu = routesWithMenu.some(route => url.startsWith(route))
+            })
+    }
 
     ngOnInit(): void {
         this.iconService.registerIcons()
