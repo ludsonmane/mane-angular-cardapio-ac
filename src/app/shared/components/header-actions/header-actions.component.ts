c61624dd39762bdb378d3dd1fe1724ba18ea0522
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-06-20 17:30:52 -0300

    feat: adiciona função onBack ao componente header-actions

--- src/app/shared/components/header-actions/header-actions.component.ts
+++ src/app/shared/components/header-actions/header-actions.component.ts
@@ -1,4 +1,6 @@
+import { Location } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-header-actions',
@@ -19,7 +21,18 @@ export class HeaderActionsComponent {
     @Output() share = new EventEmitter<void>()
     @Output() info = new EventEmitter<void>()
 
-    onBack(): void { this.back.emit() }
+    constructor(
+        private location: Location,
+        private router: Router
+    ) {}
+
+    onBack(): void {
+        if (window.history.length > 1) {
+            this.location.back()
+        } else {
+            this.router.navigate(['/home'])
+        }
+    }
 
     onFavorite(): void { this.favorite.emit() }
 
