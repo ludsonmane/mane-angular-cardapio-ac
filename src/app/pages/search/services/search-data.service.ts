0b613d3b61cb9279baee9dcca11bd57026d49cbf
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-06-29 19:24:58 -0300

    feat: consome api para dados do search

--- src/app/pages/search/services/search-data.service.ts
+++ src/app/pages/search/services/search-data.service.ts
@@ -1,17 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { CategoryModel } from '../../../shared/models/category.model';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class SearchDataService {
 
+    private apiBaseUrl: string = environment.apiBaseUrl
+    token = environment.token
+    headers = new HttpHeaders({
+        'Authorization': `Bearer ${this.token}`
+    })
+
     constructor(private http: HttpClient) { }
 
     getCategories(): Observable<any> {
-        return this.http.get<any>('data/categories.json')
+        return this.http.get<any>(this.apiBaseUrl + 'categories', { headers: this.headers })
     }
 
     getCategoryById(id: string): Observable<any> {
