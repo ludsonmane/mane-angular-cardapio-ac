fe688ce0055e5d1c8ec7778bcc4ac25e5c6a89d1
Ketlyn Santos <ketlyncastro2004@gmail.com>
2025-06-29 16:29:32 -0300

    feat: altera rotas do serviço unit-location

--- src/app/shared/services/unit-location/unit-location.service.ts
+++ src/app/shared/services/unit-location/unit-location.service.ts
@@ -1,16 +1,23 @@
 import { LocationModel } from '../../models/location.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UnitLocationService {
 
+    private apiBaseUrl: string = environment.apiBaseUrl
+    token = environment.token
+    headers = new HttpHeaders({
+        'Authorization': `Bearer ${this.token}`
+    })
+
     constructor(private http: HttpClient) { }
 
     public getLocations(): Observable<any[]> {
-        return this.http.get<any[]>('data/location.json')
+        return this.http.get<any[]>(this.apiBaseUrl + 'locations', { headers: this.headers })
     }
 }
